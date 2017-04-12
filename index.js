deb6ccb1b4868cd3ac88625b0e5f6598f485d982
Nicolás Gamboa <ngamboa@insacom.cl>
2017-04-12 15:14:32 -0300

    tabulacion

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const nightmare = Nightmare({ show: false, width: 1080 });
 const config = require('./config.json');
 
 nightmare
-  .goto('https://www.afpmodelo.cl/AFP/Home.aspx')
-  .wait('#Login_Rut')
-  .type('#Login_Rut', config.rut)
-  .type('#Login_Clave', config.pass)
-  .type('#Btn_Ingresar', '\u000d')
-  .wait('td[class="txt_total_ahorrado"]')
-  .evaluate(function () {
-      let monto_str = document.querySelectorAll('td[class="txt_total_ahorrado"]')[1].innerText;
-      let monto_int = parseInt(monto_str.replace('$ ', '').replace(/\./g, ''))
-      return { monto_str: monto_str, monto_int: monto_int };
-  })
-  .end()
-  .then(function (result) {
+.goto('https://www.afpmodelo.cl/AFP/Home.aspx')
+.wait('#Login_Rut')
+.type('#Login_Rut', config.rut)
+.type('#Login_Clave', config.pass)
+.type('#Btn_Ingresar', '\u000d')
+.wait('td[class="txt_total_ahorrado"]')
+.evaluate(function () {
+    let monto_str = document.querySelectorAll('td[class="txt_total_ahorrado"]')[1].innerText;
+    let monto_int = parseInt(monto_str.replace('$ ', '').replace(/\./g, ''))
+    return { monto_str: monto_str, monto_int: monto_int };
+})
+.end()
+.then(function (result) {
     console.log(result);
-  })
-  .catch(function (error) {
+})
+.catch(function (error) {
     console.error('failed:', error);
-  });
+});
